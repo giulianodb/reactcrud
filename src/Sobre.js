02345ae7a6e22dacedf7a79077c8f2b109460c7c
Giuliano Diego Barbarra <giulianodb@gmail.com>
2019-01-23 15:21:00 -0200

    Projeto react exemplo

---
+++ src/Sobre.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+class Sobre extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>Página SOBRE</h2>
+      </div>
+    );
+  }
+}
+
+export default Sobre;
